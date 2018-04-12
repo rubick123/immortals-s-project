7ec853fe3c68f506b6a19b2429e46584db73e413
YAYUN123 <2545757723@qq.com>
2018-04-12 20:40:32 +0800

    客服实体以及接口和测试
    第二次上传

---
+++ awm/awm-dao/src/main/java/edu/zut/cs/sowtfare/awm/people/daomain/CustomerService.java
@@ -0,0 +1,5 @@
+package edu.zut.cs.sowtfare.awm.people.daomain;
+
+public class CustomerService {
+
+}
---
+++ awm/awm-service/src/test/java/edu/zut/cs/software/awm/cloth/CustomerManagerTest.java
@@ -0,0 +1,5 @@
+package edu.zut.cs.software.awm.cloth;
+
+public class CustomerManagerTest {
+
+}
