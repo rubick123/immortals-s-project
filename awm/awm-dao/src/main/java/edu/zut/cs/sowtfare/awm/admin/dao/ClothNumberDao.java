bc730014bec08373bb4ec1445d9e89ce82216509
liuyice <1343690800@qq.com>
2018-05-15 17:03:02 +0800

    实体类

---
+++ awm/awm-dao/src/main/java/edu/zut/cs/sowtfare/awm/admin/dao/ClothNumberDao.java
@@ -0,0 +1,7 @@
+package edu.zut.cs.sowtfare.awm.admin.dao;
+
+
+import edu.zut.cs.sowtfare.awm.admin.domain.ClothNumber;
+import edu.zut.cs.sowtfare.awm.base.dao.GenericDao;
+public interface ClothNumberDao extends GenericDao<ClothNumber, Long> {
+}
