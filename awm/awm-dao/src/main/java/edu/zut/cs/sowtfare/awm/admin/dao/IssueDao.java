c3b7e5b1cec96093bd924eefecd3861d9a268de3
nayiye <1780954741@qq.com>
2018-05-15 16:48:09 +0800

    添加实体类

---
+++ awm/awm-dao/src/main/java/edu/zut/cs/sowtfare/awm/admin/dao/IssueDao.java
@@ -0,0 +1,7 @@
+package edu.zut.cs.sowtfare.awm.admin.dao;
+
+import edu.zut.cs.sowtfare.awm.admin.domain.Issue;
+import edu.zut.cs.sowtfare.awm.base.dao.GenericDao;
+
+public interface IssueDao extends GenericDao<Issue, Long> {
+}
