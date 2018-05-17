b1bfeae0c9c4865a139af5050a6509491fbcdd9f
L <3021346224@qq.com>
2018-05-17 09:55:49 +0800

    更新

---
+++ awm/awm-dao/src/main/java/edu/zut/cs/sowtfare/awm/admin/dao/FormDao.java
@@ -0,0 +1,7 @@
+package edu.zut.cs.sowtfare.awm.admin.dao;
+
+import edu.zut.cs.sowtfare.awm.admin.domain.Form;
+import edu.zut.cs.sowtfare.awm.base.dao.GenericDao;
+
+public interface FormDao extends GenericDao<Form, Long> {
+}
