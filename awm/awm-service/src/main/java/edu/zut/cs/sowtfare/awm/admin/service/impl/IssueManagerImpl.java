97d63ab6a8ac404e6e0554aa53dbc7645c765f68
YAYUN123 <2545757723@qq.com>
2018-05-17 22:05:47 +0800

    更新

---
+++ awm/awm-service/src/main/java/edu/zut/cs/sowtfare/awm/admin/service/impl/IssueManagerImpl.java
@@ -0,0 +1,29 @@
+package edu.zut.cs.sowtfare.awm.admin.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import edu.zut.cs.sowtfare.awm.admin.dao.IssueDao;
+import edu.zut.cs.sowtfare.awm.admin.domain.Issue;
+import edu.zut.cs.sowtfare.awm.admin.service.IssueManager;
+import edu.zut.cs.sowtfare.awm.base.dao.GenericDao;
+import edu.zut.cs.sowtfare.awm.base.service.impl.GenericManagerImpl;
+
+@Component
+public class IssueManagerImpl extends GenericManagerImpl<Issue, Long> implements IssueManager {
+
+	IssueDao IssueDao;
+
+	@Autowired
+	public void setIssueDao(IssueDao IssueDao) {
+		this.IssueDao =  IssueDao;
+		this.dao =  this.IssueDao;
+	}
+
+	@Override
+	public Issue findbyIssuename(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
