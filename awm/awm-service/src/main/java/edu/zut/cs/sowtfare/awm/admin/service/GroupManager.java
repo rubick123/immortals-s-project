1d80ca2a979d9998ac3a126283dbc91c30c78068
Aimer <37177717+unnatural666@users.noreply.github.com>
2018-05-18 15:37:42 +0800

    删除

--- awm/awm-service/src/main/java/edu/zut/cs/sowtfare/awm/admin/service/GroupManager.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.sowtfare.awm.admin.service;
-
-import edu.zut.cs.sowtfare.awm.admin.domain.Group;
-import edu.zut.cs.sowtfare.awm.base.service.GenericTreeManager;
-
-public interface GroupManager extends GenericTreeManager<Group, Long> {
-
-}
