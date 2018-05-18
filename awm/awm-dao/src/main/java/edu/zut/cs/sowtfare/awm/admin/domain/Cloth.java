3ab2c43a173c3db5e96aad5a0ec9b436e16dc59e
zhangxiansheng123 <767743430@qq.com>
2018-05-18 17:36:03 +0800

    xiugai

--- awm/awm-dao/src/main/java/edu/zut/cs/sowtfare/awm/admin/domain/Cloth.java
+++ awm/awm-dao/src/main/java/edu/zut/cs/sowtfare/awm/admin/domain/Cloth.java
@@ -2,8 +2,7 @@ package edu.zut.cs.sowtfare.awm.admin.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+
 import javax.persistence.Table;
 
 import edu.zut.cs.sowtfare.awm.base.domain.BaseEntity;
@@ -34,16 +33,7 @@ public class Cloth extends BaseEntity {
 	
 	
 
-	/*@ManyToOne
-	@JoinColumn(name="GROUP_ID")
-	Group group;
-	public Group getGroup() {
-		return group;
-	}
 
-	public void setGroup(Group group) {
-		this.group = group;
-	}*/
 
 
 
