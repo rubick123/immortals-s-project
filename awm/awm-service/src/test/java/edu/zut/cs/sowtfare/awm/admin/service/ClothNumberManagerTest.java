c057001e19f592ff1eb56cb374ef26ea4ce70c62
liuyice <1343690800@qq.com>
2018-05-18 10:47:43 +0800

    更新

--- awm/awm-service/src/test/java/edu/zut/cs/sowtfare/awm/admin/service/ClothNumberManagerTest.java
+++ awm/awm-service/src/test/java/edu/zut/cs/sowtfare/awm/admin/service/ClothNumberManagerTest.java
@@ -1,53 +1,37 @@
 package edu.zut.cs.sowtfare.awm.admin.service;
 
-import static org.junit.Assert.assertEquals;
-
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import edu.zut.cs.sowtfare.awm.admin.domain.ClothNumber;
 import edu.zut.cs.sowtfare.awm.base.service.GenericGenerator;
 
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = AdminServiceConfig.class)
 public class ClothNumberManagerTest extends GenericGenerator{
 
 	@Autowired
-	ClothNumberManager ClothNumberManager;
+	ClothNumberManager clothNumberManager;
+	
 	
 	
 	@Test
-	public void get_ClothNumber()
+	public void add()
 	{
-		for(int i=0;i<100;i++)
+		List<ClothNumber> list=new ArrayList<>();
+		for(int i=0;i<100;i++) 
 		{
-			ClothNumber u = new ClothNumber();
-			u.setKind("");
-			u.setColor("");
-			u.setSize("");
-			u.setNum("");
-			this.ClothNumberManager.save(u);
+			ClothNumber clothNumber=new ClothNumber();
+			clothNumber.setNum(""+i);
+			clothNumber.setColor("red");
+			clothNumber.setKind("shirt");
+			clothNumber.setSize("xxl");
+			list.add(clothNumber);
 		}
+		this.clothNumberManager.save(list);
 	}
-	
-	
-	
-	@Test
-	public void testFindAll() {
-		List<ClothNumber> all = this.ClothNumberManager.findAll();
-		assertEquals(all.size(), 100);
-	}
-	
-	@Test
-	public void testFindbykind() {
-		String kind = "world";
-		ClothNumber expected_clothnumber = new ClothNumber();
-		// expected_customerServicer.setCustomerServicername(CustomerServicername);
-		ClothNumber ClothNumber = this.ClothNumberManager.findbyClothNumber(kind);
-		// assertEquals(customerServicer.getCustomerServicername(), expected_customerServicer.CustomerServicername());
-		assertEquals(kind, expected_clothnumber);
-	}
+
 }
