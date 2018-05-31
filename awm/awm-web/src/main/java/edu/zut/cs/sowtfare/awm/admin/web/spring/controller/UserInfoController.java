ab2d72e26f11995ed5a08caef614baa2b70d5c52
Aimer <37177717+unnatural666@users.noreply.github.com>
2018-05-31 20:12:32 +0800

    增加controller模块

---
+++ awm/awm-web/src/main/java/edu/zut/cs/sowtfare/awm/admin/web/spring/controller/UserInfoController.java
@@ -0,0 +1,28 @@
+package edu.zut.cs.sowtfare.awm.admin.web.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.zut.cs.sowtfare.awm.admin.domain.UserInfo;
+import edu.zut.cs.sowtfare.awm.admin.service.UserInfoManager;
+import edu.zut.cs.sowtfare.awm.base.web.spring.controller.GenericController;
+
+@Controller
+@RequestMapping("/userinfo")
+public class UserInfoController extends GenericController<UserInfo, Long, UserInfoManager>{
+	
+	UserInfoManager userinfoManager;
+	
+	@Autowired
+	 public void setUserManager(UserInfoManager userinfoManager) {
+        this.userinfoManager = userinfoManager;
+        this.manager = this.userinfoManager;
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "user/index";
+    }
+}
+
