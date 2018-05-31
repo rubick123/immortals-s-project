563fc496359800772d932ea054007199daaef0a0
rubick123 <396641508@qq.com>
2018-05-31 22:29:22 +0800

    增加controller

---
+++ awm/awm-web/src/main/java/edu/zut/cs/sowtfare/awm/admin/web/spring/controller/SearchController.java
@@ -0,0 +1,27 @@
+package edu.zut.cs.sowtfare.awm.admin.web.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.zut.cs.sowtfare.awm.admin.domain.Search;
+import edu.zut.cs.sowtfare.awm.admin.service.SearchManager;
+import edu.zut.cs.sowtfare.awm.base.web.spring.controller.GenericController;
+
+@Controller
+@RequestMapping("/customerservicer")
+public class SearchController extends GenericController<Search, Long, SearchManager> {
+
+    SearchManager searchManager;
+
+    @Autowired
+    public void setUserManager(SearchManager searchManager) {
+        this.searchManager = searchManager;
+        this.manager = this. searchManager;
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "search/index";
+    }
+}
