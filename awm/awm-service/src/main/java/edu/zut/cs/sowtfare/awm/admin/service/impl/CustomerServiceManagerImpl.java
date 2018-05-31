bc983e62504631d4fb99f7817f4cd5d795456ddb
YAYUN123 <2545757723@qq.com>
2018-05-31 18:59:21 +0800

    删除customer类相关文件

--- awm/awm-service/src/main/java/edu/zut/cs/sowtfare/awm/admin/service/impl/CustomerServiceManagerImpl.java
+++ awm/awm-service/src/main/java/edu/zut/cs/sowtfare/awm/admin/service/impl/CustomerServiceManagerImpl.java
@@ -1,13 +1,16 @@
 package edu.zut.cs.sowtfare.awm.admin.service.impl;
 
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import edu.zut.cs.sowtfare.awm.admin.service.CustomerServicerManager;
 import edu.zut.cs.sowtfare.awm.base.service.impl.GenericManagerImpl;
 import edu.zut.cs.sowtfare.awm.admin.dao.CustomerServicerDao;
 import edu.zut.cs.sowtfare.awm.admin.domain.CustomerServicer;
-
+@Service("customerservicerManager")
+@Transactional
 @Component
 public class CustomerServiceManagerImpl extends GenericManagerImpl<CustomerServicer, Long> implements CustomerServicerManager {
 	
