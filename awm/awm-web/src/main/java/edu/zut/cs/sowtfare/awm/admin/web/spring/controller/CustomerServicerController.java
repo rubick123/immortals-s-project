b628377098e7a63c88c37182b84786b493f6c630
YAYUN123 <2545757723@qq.com>
2018-05-31 19:38:20 +0800

    更新web文件

---
+++ awm/awm-web/src/main/java/edu/zut/cs/sowtfare/awm/admin/web/spring/controller/CustomerServicerController.java
@@ -0,0 +1,26 @@
+package edu.zut.cs.sowtfare.awm.admin.web.spring.controller;
+
+import edu.zut.cs.sowtfare.awm.admin.domain.CustomerServicer;
+import edu.zut.cs.sowtfare.awm.admin.service. CustomerServicerManager;
+import edu.zut.cs.sowtfare.awm.base.web.spring.controller.GenericController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customerservicer")
+public class CustomerServicerController extends GenericController<CustomerServicer, Long, CustomerServicerManager> {
+
+    CustomerServicerManager customerServicerManager;
+
+    @Autowired
+    public void setUserManager(CustomerServicerManager customerServicerManager) {
+        this. customerServicerManager = customerServicerManager;
+        this.manager = this. customerServicerManager;
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "customerservicer/index";
+    }
+}
---
+++ awm/awm-web/src/main/java/edu/zut/cs/sowtfare/awm/main/web/spring/controller/MainController.java
@@ -0,0 +1,16 @@
+package edu.zut.cs.sowtfare.awm.main.web.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "main/index";
+    }
+
+}
+
