543b4b40bf8573a1eda398efcccf6934d10573f8
L <3021346224@qq.com>
2018-07-03 14:36:33 +0800

    添加注释

--- awm/awm-dao/src/test/java/edu/zut/cs/sowtfare/awm/admin/dao/FormDaoTest.java
+++ awm/awm-dao/src/test/java/edu/zut/cs/sowtfare/awm/admin/dao/FormDaoTest.java
@@ -9,9 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import edu.zut.cs.sowtfare.awm.admin.domain.Form;
 import edu.zut.cs.sowtfare.awm.base.dao.GenericDaoTestCase;
 /**
- * 
+ * This is a test class,used for testing form entities
+ * @ClassName FormDaoTest
  * @author LGY
- *
+ * @time 18/7/3 14:19
  */
 public class FormDaoTest extends GenericDaoTestCase<Long, Form, FormDao> {
 
