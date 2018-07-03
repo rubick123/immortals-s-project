79ab60339c8d245784ebc8a835b228c671bca3ec
L <3021346224@qq.com>
2018-07-03 14:35:22 +0800

    添加注释

--- awm/awm-dao/src/main/java/edu/zut/cs/sowtfare/awm/admin/dao/FormDao.java
+++ awm/awm-dao/src/main/java/edu/zut/cs/sowtfare/awm/admin/dao/FormDao.java
@@ -3,9 +3,10 @@ package edu.zut.cs.sowtfare.awm.admin.dao;
 import edu.zut.cs.sowtfare.awm.admin.domain.Form;
 import edu.zut.cs.sowtfare.awm.base.dao.GenericDao;
 /**
- * This is an interface class,use for DAO
+ * This is an interface class,form for DAO
+ * @ClassName FormDao
  * @author LGY
- *
+ * @Time 18/7/3 14:21
  */
 public interface FormDao extends GenericDao<Form, Long> {
 }
