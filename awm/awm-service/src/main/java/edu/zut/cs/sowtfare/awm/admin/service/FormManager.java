170f8ea34ed45b8a7d12aa35d8516d37bc50aa59
L <3021346224@qq.com>
2018-07-03 14:37:45 +0800

    添加注释

--- awm/awm-service/src/main/java/edu/zut/cs/sowtfare/awm/admin/service/FormManager.java
+++ awm/awm-service/src/main/java/edu/zut/cs/sowtfare/awm/admin/service/FormManager.java
@@ -5,9 +5,10 @@ import java.util.List;
 import edu.zut.cs.sowtfare.awm.admin.domain.Form;
 import edu.zut.cs.sowtfare.awm.base.service.GenericManager;
 /**
- * 
+ * This is an interface,There are two methods, findAll() and findByFormname
+ * @ClassName FormManager
  * @author LGY
- *
+ * @Time 18/7/3 14:29
  */
 public interface FormManager extends GenericManager<Form, Long> {
 
